6b3eaa162df418a5296ab3ac5807e69500a7780e
Will Hawks <willkhawks@gmail.com>
2022-04-18 15:39:39 -0500

    add port env option

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function startApolloServer(typeDefs, resolvers) {
       credentials: true,
     },
   });
-  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+  await new Promise(resolve => httpServer.listen({ port: process.env.PORT || 4000 }, resolve));
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
